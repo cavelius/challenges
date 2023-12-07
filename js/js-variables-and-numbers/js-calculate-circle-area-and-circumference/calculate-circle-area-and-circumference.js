964f930ca172dac3fdd686ad9e84a38c0ded7ebb
Sofia Cavelius <sofiacavelius@web.de>
2023-12-07 16:22:20 +0100

    add cicle task

---
+++ js/js-variables-and-numbers/js-calculate-circle-area-and-circumference/calculate-circle-area-and-circumference.js
@@ -0,0 +1,18 @@
+let radius = 5;
+let circleArea = Math.PI * Math.pow(radius, 2);
+
+console.log(circleArea);
+
+let circumference = 2 * Math.PI * radius;
+
+console.log(circumference);
+
+console.log(
+  "For a circle with a radius of",
+  radius,
+  "units The area is approximately",
+  circleArea,
+  " square units.The circumference is approximately",
+  circumference,
+  " units."
+);

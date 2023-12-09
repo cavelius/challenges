a8d8321a25ebf80e4f36578e99e0ea1e03cbbf25
Sofia Cavelius <sofiacavelius@web.de>
2023-12-09 18:24:10 +0100

    add darkmode

--- js/js-basic/js-basics_dark-mode/js/index.js
+++ js/js-basic/js-basics_dark-mode/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const btnDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+btnDarkMode.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+const btnlightMode = document.querySelector('[data-js="light-mode-button"]');
+btnlightMode.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+const btnToggle = document.querySelector('[data-js="toggle-button"]');
+btnToggle.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});

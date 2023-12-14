9c42b92d6dc660cb67d2865920fdc403b1d85594
Sofia Cavelius <sofiacavelius@web.de>
2023-12-14 11:12:18 +0100

    Handle form submit done

--- js/js-forms/js-forms_handle-form-submit/js/index.js
+++ js/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,16 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log("event targe elements: ", event.target.elements);
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log("data: ", data);
+
+  event.target.reset();
+  document.getElementById("first-name").focus();
+});

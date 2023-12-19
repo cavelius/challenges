7b28bd86a0d2feb5585d78c19b38868ebfdd2170
Sofia Cavelius <sofiacavelius@web.de>
2023-12-19 15:31:10 +0100

    add named vs anonymous

--- js/js-callback-functions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js/js-callback-functions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -6,3 +6,15 @@ function handleUserLogin(onSuccess) {
 }
 
 // Call handleUserLogin below!
+
+function showWelcomeMessage() {
+  console.log("Welcome! You are logged in now.");
+}
+
+handleUserLogin(() => {
+  showWelcomeMessage();
+});
+
+handleUserLogin(function () {
+  showWelcomeMessage();
+});

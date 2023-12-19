2d6faa92ccdc0b576112dcab4bf924653a2a4bab
Sofia Cavelius <sofiacavelius@web.de>
2023-12-19 17:31:27 +0100

    add task multiple-functions

--- js/js-callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ js/js-callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -9,3 +9,32 @@ function handleUserLogin(onSuccess, onError, userName) {
 }
 
 // The exercise starts here!
+
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName} you are logged in now as ${userRole}.`);
+}
+
+// Write a function with the "function" keyword named "showErrorMessage".
+// The function should accept one parameter "errorMessage" The function should
+// log the following message to the console: "Login error: ${errorMessage}"
+
+function showErrorMessage(errorMessage) {
+  console.log(`Login error: ${errorMessage}`);
+}
+
+// Call the function "handleUserLogin" and pass three arguments in this order:
+
+// callback function: "showWelcomeMessage"
+// callback function: "showErrorMessage"
+// user name string: "Jane Doe"
+//  (You should see the log from "showWelcomeMessage" in the console)
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, "Jane Doe");
+
+// Call the function "handleUserLogin" and pass three arguments in this order:
+// callback function: "showWelcomeMessage"
+// callback function: "showErrorMessage"
+// user name string: "John Doe"
+// (You should see the log from "showErrorMessage" in the console)
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, "John Doe");

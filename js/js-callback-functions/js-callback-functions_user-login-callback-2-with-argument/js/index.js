9c68d76c32a330a7fbabad6f782b219c679ad99c
Sofia Cavelius <sofiacavelius@web.de>
2023-12-19 16:29:38 +0100

    add task functions-with-one-argument

--- js/js-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ js/js-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,28 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+function showWelcomeMessage(userName) {
+  console.log(`Welcome ${userName} You are logged in now.`);
+}
+
+// Write a function with the "function" keyword named
+// "showWelcomeMessage" Call "handleUserLogin" and pass your
+// callback function as the first argument.
+// (You should see the log from your callback message)
+
+// Call "handleUserLogin" again but this time write the
+// callback function as an anonymous function inside the
+// function call (i.e. between the round brackets).
+//  (You should see the log from your callback message)
+// });
+
+// handleUserLogin((userName) => {
+//   showWelcomeMessage(userName);
+// });
+
+handleUserLogin(function (userName) {
+  showWelcomeMessage(userName);
+});
+
+handleUserLogin(showWelcomeMessage);

6d811360c74b28cec4c85780acb2bded918e4e65
Sofia Cavelius <sofiacavelius@web.de>
2024-01-06 21:47:54 +0100

    add js-functions-2_arrow-functions

--- js/06_js-functions-2/js-functions-2_arrow-functions/js/index.js
+++ js/06_js-functions-2/js-functions-2_arrow-functions/js/index.js
@@ -5,7 +5,23 @@ Rewrite the following functions as arrow functions.
 Use implicit returns when possible
 */
 
-function getCurrentHour() {
+// function getCurrentHour() {
+//   const now = new Date();
+//   const currentHour = now.getHours();
+//   if (currentHour === 0) {
+//     return "12am";
+//   } else if (currentHour === 12) {
+//     return "12pm";
+//   } else if (currentHour <= 11) {
+//     return currentHour + "am";
+//   } else {
+//     return currentHour - 12 + "pm";
+//   }
+// }
+
+// console.log(getCurrentHour());
+
+let getCurrentHour = () => {
   const now = new Date();
   const currentHour = now.getHours();
   if (currentHour === 0) {
@@ -17,21 +33,44 @@ function getCurrentHour() {
   } else {
     return currentHour - 12 + "pm";
   }
-}
+};
 
-function getVectorLength(x, y, z) {
+console.log(getCurrentHour());
+
+// function getVectorLength(x, y, z) {
+//   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+// }
+
+let getVectorLength = (x, y, z) => {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+};
+console.log(getVectorLength(2, 3, 4));
 
-function cleanInput(string) {
+// function cleanInput(string) {
+//   return string.toLowerCase().trim();
+// }
+
+cleanInput = (string) => {
   return string.toLowerCase().trim();
-}
-
+};
+console.log(cleanInput(" Hello I am Sofia "));
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+// const isOddAndSmall = (number) => {
+//   if (number > 10) {
+//     return false;
+//   }
+
+//   if (number % 2 === 0) {
+//     return false;
+//   }
+
+//   return true;
+// };
+
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -41,8 +80,20 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
+console.log(isOddAndSmall(7));
 
-const add3 = (a, b, c) => a + b + c;
+// const add3 = (a, b, c) => a + b + c;
 
-const repeat10 = (string) => string.repeat(10);
+function add3(a, b, c) {
+  return a + b + c;
+}
+console.log(add3(1, 2, 2));
+
+// const repeat10 = (string) => string.repeat(10);
+
+function repeat10(string) {
+  return string.repeat(10);
+}
+
+console.log(repeat10("Hello"));

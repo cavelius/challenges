e47f4f3f7adcf34aa385cf293d510513097c3907
Sofia Cavelius <sofiacavelius@web.de>
2024-01-08 16:33:25 +0100

    clean file

--- js/18_js-fetch/01_js-fetch_star-wars-console/js/index.js
+++ js/18_js-fetch/01_js-fetch_star-wars-console/js/index.js
@@ -5,7 +5,6 @@ const url = "https://swapi.dev/api/people";
 async function fetchData() {
   try {
     const response = await fetch(url);
-
     const data = await response.json();
 
     console.log(data);

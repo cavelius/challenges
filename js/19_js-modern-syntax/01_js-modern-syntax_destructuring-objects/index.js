97dfd7f98d6c77f8716e5291c2f43f84b39a1aef
Sofia Cavelius <sofiacavelius@web.de>
2024-01-09 11:10:13 +0100

    done js-modern-syntax_destructuring-objects

--- js/19_js-modern-syntax/01_js-modern-syntax_destructuring-objects/index.js
+++ js/19_js-modern-syntax/01_js-modern-syntax_destructuring-objects/index.js
@@ -12,6 +12,9 @@ const company = {
 
 // example: export const {value1} = myObject;
 
+const { course } = company;
+console.log(course);
+
 // EXERCISE 2
 // We have an object called 'user'.
 // Write the destructuring assignment that reads:
@@ -19,7 +22,10 @@ const company = {
 //  2.2: 'years' property into the variable 'age'.
 //  2.3: 'isAdmin' property into the variable 'isAdmin' (false, if no such property exists)
 
-const user = { name: "John", years: 30 };
+// const user = { name: "John", years: 30 };
+// const { name, years: age, isAdmin = false } = user;
+
+// console.log(name, age, isAdmin);
 
 // EXERCISE 3
 // Extract the variables from the dog object and
@@ -31,6 +37,9 @@ const dog = {
   age: 5,
 };
 
+const { name: dogName, breed: dogBreed, age: dogAge } = dog;
+console.log(dogName, dogBreed, dogAge);
+
 // EXERCISE 4
 // Extract the 'lastName' property from the person object as "personLastName".
 // Store all other properties in an object called "moreInformation".
@@ -40,3 +49,10 @@ const person = {
   age: 24,
   firstName: "Alex",
 };
+
+// const { lastName: personLastName, age, firstname } = person;
+// console.log(personLastName);
+
+const { lastName: personLastName, ...moreInformation } = person;
+console.log(moreInformation);
+console.log(personLastName);

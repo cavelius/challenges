1f75e3c0dca16877a2e549a5321ab95e4a401484
Sofia Cavelius <sofiacavelius@web.de>
2024-01-16 15:03:51 +0100

    add

---
+++ js/24_react-nesting/03_react-nesting_navigation/src/components/Navigation.js
@@ -0,0 +1,15 @@
+export default function Navigation({ children }) {
+  return <nav>{children}</nav>;
+}
+// *  */
+//     <nav>
+//     <a className="navigation__link" href="#home">
+//       Home
+//     </a>
+//     <a className="navigation__link" href="#about">
+//       About
+//     </a>
+//     <a className="navigation__link" href="#impressum">
+//       Impressum
+//     </a>
+//   </nav>

5d21324ea4a352298d92523b1b361db4ffebd3db
Sofia Cavelius <sofiacavelius@web.de>
2024-01-30 22:34:35 +0100

    add task

---
+++ js/38_react-data-fetching/react-data-fetching_star-wars/pages/_app.js
@@ -0,0 +1,16 @@
+import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+// const fetcher = (url) => fetch(url).then((response) => response.json());
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </>
+  );
+}

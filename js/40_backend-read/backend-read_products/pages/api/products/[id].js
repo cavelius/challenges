b7c771787e86942daac5623f6b5b4e9d4b935a0c
Sofia Cavelius <sofiacavelius@web.de>
2024-02-12 15:24:45 +0100

    done backend-read

--- js/40_backend-read/backend-read_products/pages/api/products/[id].js
+++ js/40_backend-read/backend-read_products/pages/api/products/[id].js
@@ -1,9 +1,29 @@
-import { products } from "../../../lib/products";
+// import { products } from "../../../lib/products";
 
-export default function handler(request, response) {
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/Products";
+
+// export default async function handler(request, response) {
+//   await dbConnect()
+//   const { id } = request.query;
+
+//   const product = products.find((product) => product.id === id);
+
+//   if (!product) {
+//     return response.status(404).json({ status: "Not Found" });
+//   }
+
+//   response.status(200).json(product);
+// }
+
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  const product = await Product.findById(id);
+
+  console.log(product);
 
   if (!product) {
     return response.status(404).json({ status: "Not Found" });
--- js/40_backend-read/backend-read_products/pages/api/products/index.js
+++ js/40_backend-read/backend-read_products/pages/api/products/index.js
@@ -1,5 +1,18 @@
-import { products } from "../../../lib/products";
+// import { products } from "../../../lib/products";
+import { log } from "console";
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/Products";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    try {
+      const products = await Product.find();
+      console.log("===========", products);
+      response.status(200).json(products);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
